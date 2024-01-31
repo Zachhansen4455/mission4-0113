58746eb42efec758f179c18f3c44565976138f66
Zachhansen4455 <120439561+Zachhansen4455@users.noreply.github.com>
2024-01-31 10:21:12 -0700

    print board method

--- supportingClass.cs
+++ supportingClass.cs
@@ -1,8 +1,23 @@
 ﻿using System;
 
-public class Class1
+namespace TicTacToe
 {
-	public Class1()
-	{
-	}
+    public class SupportingClass
+    {
+        // print board method
+        public void PrintBoard(char[,] board)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write(board[x, y] == '\0' ? "  " : $"{board[x, y]} ");
+                    if (y < 2) Console.Write("|");
+                }
+                Console.WriteLine();
+                if (x < 2) Console.WriteLine("-----");
+            }
+        }
+        
+    }
 }
